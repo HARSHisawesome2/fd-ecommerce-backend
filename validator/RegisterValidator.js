26a902cdd21d002cc9d9570e808bb52b6f230a69
Frederick-88 <frederickfd88@gmail.com>
2020-06-28 22:02:02 +0700

    register validator.js done

--- validator/RegisterValidator.js
+++ validator/RegisterValidator.js
@@ -9,7 +9,6 @@ module.exports = function validateRegisterInput(data) {
       email: "",
       phoneNumber: "",
       password: "",
-      confirmPassword: "",
     },
   };
   //  avoid error pop-up
@@ -25,13 +24,75 @@ module.exports = function validateRegisterInput(data) {
       ...errors.message,
       username: "Username is required.",
     };
-    //  the result without "!" is true. we're trying to get the reverse result
+    // the result without "!" is true. we're trying to get the reverse result
     // isAlphanumeric = we dont want "@", "#", etc. Only want text & number.
   } else if (!Validator.isAlphanumeric(data.username)) {
     errors.status = 401;
     errors.message = {
       ...errors.message,
-      username: "Username is invalid",
+      username: "Username is invalid.",
+    };
+  } else if (!Validator.isLowercase(data.username)) {
+    errors.status = 401;
+    errors.message = {
+      ...errors.message,
+      username: "Username must be lowercase only.",
     };
   }
+
+  // Email validator
+  if (Validator.isEmpty(data.email)) {
+    errors.status = 401;
+    errors.message = {
+      ...errors.message,
+      email: "Email is required.",
+    };
+  } else if (!Validator.isEmail(data.email)) {
+    errors.status = 401;
+    errors.message = {
+      ...errors.message,
+      email: "Email is invalid.",
+    };
+  }
+
+  // PhoneNumber validator
+  if (Validator.isEmpty(data.phoneNumber)) {
+    errors.status = 401;
+    errors.message = {
+      ...errors.message,
+      phoneNumber: "PhoneNumber is required.",
+    };
+  } else if (!Validator.isMobilePhone(data.phoneNumber)) {
+    errors.status = 401;
+    errors.message = {
+      ...errors.message,
+      phoneNumber: "PhoneNumber is invalid.",
+    };
+  }
+
+  // Password validator
+  if (Validator.isEmpty(data.password)) {
+    errors.status = 401;
+    errors.message = {
+      ...errors.message,
+      password: "Password is required.",
+    };
+  } else if (
+    !Validator.isLength(data.password, {
+      min: 8,
+      max: 30,
+    })
+  ) {
+    (errors.status = 401),
+      (errors.message = {
+        ...errors.message,
+        password:
+          "Password must be at least 8 characters either under 30 characters.",
+      });
+  }
+  return {
+    errors,
+    // isValid = no errors occur
+    isValid: isEmpty(errors.status),
+  };
 };
